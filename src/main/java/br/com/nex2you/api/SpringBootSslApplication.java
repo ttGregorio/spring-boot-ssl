8a343363857968022ebd32f7c0fb8741c4e06dfa
Thiago Gregorio <thiagotgregorio@gmail.com>
2021-06-29 00:24:42 -0300

    Inclusão dos fontes do projeto de modelo de api certificada

---
+++ src/main/java/br/com/nex2you/api/SpringBootSslApplication.java
@@ -0,0 +1,13 @@
+package br.com.nex2you.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSslApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/nex2you/api/SpringBootSslApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nex2you.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
